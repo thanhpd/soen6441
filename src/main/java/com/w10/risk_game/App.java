83a9c25f1d091c4834203a9604927dc9dd8d7edb
Darlene-Naz <46684660+Darlene-Naz@users.noreply.github.com>
2023-09-18 16:57:50 -0400

    updated maven folder structure

---
+++ src/main/java/com/w10/risk_game/App.java
@@ -0,0 +1,13 @@
+package com.w10.risk_game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

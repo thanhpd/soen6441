04eca6611d7e35d46a99ff3f7e286345b5ca0054
Omnia <omnia.alam@gmail.com>
2023-09-21 23:41:11 -0400

    Remove Country, Continent

--- src/main/java/com/w10/risk_game/utils/MapEditor.java
+++ src/main/java/com/w10/risk_game/utils/MapEditor.java
@@ -7,43 +7,43 @@ import com.w10.risk_game.models.Country;
 import com.w10.risk_game.models.GameMap;
 
 public class MapEditor {
-    private GameMap originalMap;
-ArrayList<Country> countries;
-ArrayList<Continent> continents;
+  private GameMap originalMap;
+  ArrayList<Country> countries;
+  ArrayList<Continent> continents;
 
-    public MapEditor(GameMap originalMap){
-        this.originalMap=originalMap;
-        countries = new ArrayList<>(originalMap.getCountries().values());
-    }
+  public MapEditor(GameMap originalMap) {
+    this.originalMap = originalMap;
+    countries = new ArrayList<>(originalMap.getCountries().values());
+    continents = new ArrayList<>(originalMap.getContinents().values());
+  }
 
-public void addCounrty(int countryId,String countryName, int continentId){
-    Country country = new Country(countryId, countryName,continentId);
+  public void addCounrty(int countryId, String countryName, int continentId) {
+    Country country = new Country(countryId, countryName, continentId);
     countries.add(country);
   }
-  public void addContinent(int continentId, String continentName){
-    Continent continent=new Continent(continentId,continentName);
+
+  public void addContinent(int continentId, String continentName) {
+    Continent continent = new Continent(continentId, continentName);
     continents.add(continent);
-
   }
 
-  public void addNeighbor(int countryId,int neighbourCountryId){
-    Country country= new Country();
+  public void addNeighbor(int countryId, int neighbourCountryId) {
    
   }
 
-  public void removeCountry(int countryId){
-
-  }
-  public void removeContinent(int continentId){
-
-  }
-  public void removeNeighbour(int countryId, int neighbourCountryId){
-      
+  public void removeCountry(int countryId) {
+    countries.removeIf(country -> countryId == country.getCountryId());
   }
 
-   
-  public void validateMap(){
+  public void removeContinent(int continentId) {
+    continents.removeIf(continent -> continentId == continent.getContinentId());
+  }
+
+  public void removeNeighbour(int countryId, int neighbourCountryId) {
 
   }
-    
+
+  public void validateMap() {
+
+  }
 }

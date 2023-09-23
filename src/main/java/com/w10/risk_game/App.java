e044a0cd7a1dd00db73f213f0187919060acb9a8
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-09-23 18:07:48 -0400

    added console commands to root file

--- src/main/java/com/w10/risk_game/App.java
+++ src/main/java/com/w10/risk_game/App.java
@@ -1,5 +1,7 @@
 package com.w10.risk_game;
 
+import java.util.Scanner;
+
 /**
  * Initialize the Application
  */
@@ -17,5 +19,28 @@ public class App {
 	 */
 	private static void printToTerminal() {
 		System.out.println("Hello World!");
+	}
+
+	public void start(){ 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("");
+        System.out.println("==================================");
+        System.out.println("\t\t\t Warzone");
+        System.out.println("==================================");
+        System.out.println("\t\t\t Main Menu");
+        System.out.println("\t=======================");
+        System.out.println("\t\t 1. New Game");
+        System.out.println("\t\t 2. Load Game");
+        System.out.println("\t\t 3. Single Game Mode");
+        System.out.println("\t\t 4. Simulation Mode");
+        System.out.println("\t\t 5. Exit");
+        System.out.println("\t=======================");
+        System.out.println("\t\tSelect the option");
+        System.out.println("==================================");
+
+		int option = scanner.nextInt();
+
+		
 	}
 }

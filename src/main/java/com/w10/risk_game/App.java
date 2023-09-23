acf6b5ba7ec9684870dc6c9a54d859f40a418189
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-09-23 18:34:26 -0400

    fixed formatting

--- src/main/java/com/w10/risk_game/App.java
+++ src/main/java/com/w10/risk_game/App.java
@@ -7,53 +7,50 @@ import java.util.Scanner;
  */
 public class App {
 	/**
-	 * @param args
-	 *            passed in arguments for the application
+	 * @param args passed in arguments for the application
 	 */
 	public static void main(String[] args) {
 		App.printToTerminal();
 	}
 
 	/**
-	 * This test method print out a line in the terminal
+	 * This test method prints out a line in the terminal
 	 */
 	private static void printToTerminal() {
 		System.out.println("Hello World!");
 		start();
 	}
 
-	public static void start(){ 
+	public static void start() {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("");
-        System.out.println("==================================");
-        System.out.println("\t\t\t Warzone");
-        System.out.println("==================================");
-        System.out.println("\t\t\t Main Menu");
-        System.out.println("\t=======================");
-        System.out.println("\t\t 1. New Game");
-        System.out.println("\t\t 2. Load Game");
-        System.out.println("\t\t 3. Exit");
-        System.out.println("\t=======================");
-        System.out.println("\t\tSelect the option");
-        System.out.println("==================================");
+		System.out.println("==================================");
+		System.out.println("\t\t\t Warzone");
+		System.out.println("==================================");
+		System.out.println("\t\t\t Main Menu");
+		System.out.println("\t=======================");
+		System.out.println("\t\t 1. New Game");
+		System.out.println("\t\t 2. Load Game");
+		System.out.println("\t\t 3. Exit");
+		System.out.println("\t=======================");
+		System.out.println("\t\tSelect the option");
+		System.out.println("==================================");
 
 		int option = scanner.nextInt();
 
 		switch (option) {
-            case 1:
-                System.out.println("Starting a New Game");
-                break;
-            case 2:
-                System.out.println("Loading a Game");
-                break;
-            case 3:
-                System.out.println("Exiting the Game");
-                break;
-            default:
-                System.out.println("Invalid Option. Please choose a valid option.");
-        }
-
-		
+			case 1:
+				System.out.println("Starting a New Game");
+				break;
+			case 2:
+				System.out.println("Loading a Game");
+				break;
+			case 3:
+				System.out.println("Exiting the Game");
+				break;
+			default:
+				System.out.println("Invalid Option. Please choose a valid option.");
+		}
 	}
 }

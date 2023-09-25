e62f3178f4dcfa0c0cd1ae161a1a5ec14f024004
Darlene-Naz <46684660+Darlene-Naz@users.noreply.github.com>
2023-09-25 19:39:42 -0400

    added javadocs and updated code as per specified format

--- src/main/java/com/w10/risk_game/models/Player.java
+++ src/main/java/com/w10/risk_game/models/Player.java
@@ -2,64 +2,149 @@ package com.w10.risk_game.models;
 
 import java.util.List;
 
+/**
+ * The Player class represents a player in this game, with properties such as
+ * name, countries owned, orders, and leftover armies, as well as methods to
+ * manipulate these properties.
+ *
+ * @author Darlene-Naz
+ */
 public class Player {
-	private String name;
-	private List<Country> countriesOwned;
-	private List<Order> orders;
-	private int leftoverArmies;
+	private String d_name;
+	private List<Country> d_countriesOwned;
+	private List<Order> d_orders;
+	private int d_leftoverArmies;
 
-	public Player(String name, List<Country> countriesOwned, List<Order> orders, int leftoverArmies) {
-		this.name = name;
-		this.countriesOwned = countriesOwned;
-		this.orders = orders;
-		this.leftoverArmies = leftoverArmies;
+	/**
+	 * The `Player` constructor is initializing a new instance of the `Player` class
+	 * with the provided parameters. It sets the player's name (`d_name`), the list
+	 * of countries owned by the player (`d_countriesOwned`), the list of orders for
+	 * the player (`d_orders`), and the number of leftover armies of the player
+	 * (`d_leftoverArmies`).
+	 *
+	 * @param p_name
+	 * @param p_countriesOwned
+	 * @param p_orders
+	 * @param p_leftoverArmies
+	 */
+	public Player(String p_name, List<Country> p_countriesOwned, List<Order> p_orders, int p_leftoverArmies) {
+		this.d_name = p_name;
+		this.d_countriesOwned = p_countriesOwned;
+		this.d_orders = p_orders;
+		this.d_leftoverArmies = p_leftoverArmies;
 	}
 
+	/**
+	 * The function returns the name of the player.
+	 *
+	 * @return The method is returning the player's name.
+	 */
 	public String getName() {
-		return name;
+		return d_name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	/**
+	 * The function sets the name of the player.
+	 *
+	 * @param p_name The parameter "p_name" is a String that represents the name of
+	 *               the player.
+	 */
+	public void setName(String p_name) {
+		this.d_name = p_name;
 	}
 
+	/**
+	 * The getCountriesOwned function returns a list of countries owned by the
+	 * player.
+	 *
+	 * @return A List of Country objects is being returned.
+	 */
 	public List<Country> getCountriesOwned() {
-		return countriesOwned;
+		return d_countriesOwned;
 	}
 
-	public void setCountriesOwned(List<Country> countriesOwned) {
-		this.countriesOwned = countriesOwned;
+	/**
+	 * The function sets the list of countries owned by a player.
+	 *
+	 * @param p_countriesOwned This parameter is a List of objects of type Country.
+	 *                         It represents the countries that are owned by a
+	 *                         player.
+	 */
+	public void setCountriesOwned(List<Country> p_countriesOwned) {
+		this.d_countriesOwned = p_countriesOwned;
 	}
 
+	/**
+	 * The function returns the player's list of orders.
+	 *
+	 * @return A List of Order objects is being returned.
+	 */
 	public List<Order> getOrders() {
-		return orders;
+		return d_orders;
 	}
 
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
+	/**
+	 * The function sets the list of orders for a given player.
+	 *
+	 * @param p_orders The parameter "p_orders" is a List of Order objects of a
+	 *                 player.
+	 */
+	public void setOrders(List<Order> p_orders) {
+		this.d_orders = p_orders;
+	}
+
+	/**
+	 * The function returns the number of leftover armies.
+	 *
+	 * @return The method is returning an integer value, specifically the value of
+	 *         the variable "d_leftoverArmies".
+	 */
+	public int getLeftoverArmies() {
+		return d_leftoverArmies;
+	}
+
+	/**
+	 * The function sets the value of the "leftoverArmies" variable.
+	 *
+	 * @param p_leftoverArmies The parameter "p_leftoverArmies" is an integer that
+	 *                         represents the number of leftover armies.
+	 */
+	public void setLeftoverArmies(int p_leftoverArmies) {
+		this.d_leftoverArmies = p_leftoverArmies;
+	}
+
+	/**
+	 * The function "deployArmies" subtracts the specified number of armies from the
+	 * leftover armies.
+	 *
+	 * @param p_num The parameter "p_num" represents the number of armies that are
+	 *              being deployed.
+	 */
+	public void deployArmies(int p_num) {
+		this.d_leftoverArmies -= p_num;
+	}
+
+	/**
+	 * The function adds a specified number of armies to a player's leftoverArmies.
+	 *
+	 * @param p_num The parameter "p_num" represents the number of armies that will
+	 *              be added to the existing number of leftover armies.
+	 */
+	public void addArmies(int p_num) {
+		this.d_leftoverArmies += p_num;
 	}
 
 	public void issueOrder() {
 		// todo - to add an order to the list of orders held by the player
 	}
 
+	/**
+	 * The function "nextOrder" returns and removes the first element from a list of
+	 * player's orders.
+	 *
+	 * @return The method is returning an object of type Order.
+	 */
 	public Order nextOrder() {
-		return orders.remove(0);
-	}
-
-	public int getLeftoverArmies() {
-		return leftoverArmies;
-	}
-
-	public void setLeftoverArmies(int leftoverArmies) {
-		this.leftoverArmies = leftoverArmies;
-	}
-
-	public void deployArmies(int num) {
-		this.leftoverArmies = leftoverArmies - num;
-	}
-
-	public void addArmies(int num) {
-		this.leftoverArmies = leftoverArmies + num;
+		return d_orders.remove(0);
 	}
 }

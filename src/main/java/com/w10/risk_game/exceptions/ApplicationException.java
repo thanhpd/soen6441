1d780f2413e674bf318f8485f9b59e34132c6839
Darlene-Naz <46684660+Darlene-Naz@users.noreply.github.com>
2023-09-26 01:13:08 -0400

    spotless formatting update

--- src/main/java/com/w10/risk_game/exceptions/ApplicationException.java
+++ src/main/java/com/w10/risk_game/exceptions/ApplicationException.java
@@ -1,9 +1,9 @@
 package com.w10.risk_game.exceptions;
 
 /**
- * The ApplicationException is a custom exception class that wraps
- * all checked standard Java exceptions and can be used to handle
- * risk-game-specific errors in Java.
+ * The ApplicationException is a custom exception class that wraps all checked
+ * standard Java exceptions and can be used to handle risk-game-specific errors
+ * in Java.
  *
  * @author Darlene-Naz
  * @version 1.0

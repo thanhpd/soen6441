b25abc9c8124a8097b36a2746dccb15556559831
Darlene-Naz <46684660+Darlene-Naz@users.noreply.github.com>
2023-09-26 02:17:17 -0400

    updated continent with additional data members

--- src/main/java/com/w10/risk_game/models/Continent.java
+++ src/main/java/com/w10/risk_game/models/Continent.java
@@ -1,40 +1,64 @@
 package com.w10.risk_game.models;
 
+import java.util.List;
+
 public class Continent {
-	private int continentId;
-	private String continentName;
+	private int d_continentId;
+	private String d_continentName;
+	private int d_armyCount;
+	private List<Country> d_countries;
+	private List<Country> d_neighborCountries;
 
 	public Continent() {
-
 	}
 
-	public Continent(int continentId, String continentName) {
-		this.continentId = continentId;
-		this.continentName = continentName;
+	public Continent(int p_continentId, String p_continentName, int p_armyCount) {
+		this.d_continentId = p_continentId;
+		this.d_continentName = p_continentName;
+		this.d_armyCount = p_armyCount;
 	}
 
-	// Getter
 	public int getContinentId() {
-		return continentId;
+		return d_continentId;
 	}
 
-	// Setter
-
-	public void setContinentId(int continentId) {
-		this.continentId = continentId;
+	public void setContinentId(int d_continentId) {
+		this.d_continentId = d_continentId;
 	}
 
-	// Getter
 	public String getContinentName() {
-		return continentName;
+		return d_continentName;
 	}
 
-	// Setter
-	public void setContinentName(String continentName) {
-		this.continentName = continentName;
+	public void setContinentName(String d_continentName) {
+		this.d_continentName = d_continentName;
 	}
+
+	public int getArmyCount() {
+		return d_armyCount;
+	}
+
+	public void setArmyCount(int d_armyCount) {
+		this.d_armyCount = d_armyCount;
+	}
+
+	public List<Country> getCountries() {
+		return d_countries;
+	}
+
+	public void setCountries(List<Country> d_countries) {
+		this.d_countries = d_countries;
+	}
+
+	public List<Country> getNeighborCountries() {
+		return d_neighborCountries;
+	}
+
+	public void setNeighborCountries(List<Country> d_neighborCountries) {
+		this.d_neighborCountries = d_neighborCountries;
+	}
+
 	public String toString() {
-		return continentId + " " + continentName;
+		return d_continentId + " " + d_continentName;
 	}
-
 }

98bf7bec7d02692228dbd27b998f54a24c775948
Darlene-Naz <46684660+Darlene-Naz@users.noreply.github.com>
2023-09-28 19:56:04 -0400

    updated app import

--- src/main/java/com/w10/risk_game/App.java
+++ src/main/java/com/w10/risk_game/App.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import com.w10.risk_game.models.GameEngine;
 import com.w10.risk_game.utils.Constants;
-import com.w10.risk_game.utils.MapValidator;
 
 /**
  * Initialize the Application

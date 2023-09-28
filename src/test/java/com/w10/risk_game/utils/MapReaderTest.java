4c4e5767ad83e99f527d5b68cf267f5f826b7fa6
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-09-28 11:43:04 -0400

    fixed variable names

--- src/test/java/com/w10/risk_game/utils/MapReaderTest.java
+++ src/test/java/com/w10/risk_game/utils/MapReaderTest.java
@@ -6,13 +6,13 @@ import com.w10.risk_game.models.GameMap;
 
 public class MapReaderTest {
 
-	MapReader mapReader = new MapReader();
+	MapReader d_mapReader = new MapReader();
 
 	@Test
 	public void testLoadMapFile() {
-		String mapFilename = "europe.map";
-		GameMap gameMap = mapReader.loadMapFile(mapFilename);
-		assertNotNull(gameMap);
+		String l_mapFilename = "europe.map";
+		GameMap d_gameMap = d_mapReader.loadMapFile(l_mapFilename);
+		assertNotNull(d_gameMap);
 	}
 
 }

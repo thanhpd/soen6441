b212d51216f54453ac901926c44537c57f433693
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-09-27 20:49:24 -0400

    Update MapReaderTest.java

--- src/test/java/com/w10/risk_game/utils/MapReaderTest.java
+++ src/test/java/com/w10/risk_game/utils/MapReaderTest.java
@@ -10,11 +10,8 @@ public class MapReaderTest {
 
 	@Test
 	public void testLoadMapFile() {
-		// Provide a test map file (e.g., "testmap.map") in your resources folder
 		String mapFilename = "europe.map";
 		GameMap gameMap = mapReader.loadMapFile(mapFilename);
-
 		assertNotNull(gameMap);
-		// Add assertions to verify the correctness of the loaded GameMap object
 	}
 }

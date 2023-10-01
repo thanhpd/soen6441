7041d3b62a290ba5c483c6e857c43b6048ae6701
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-09-30 20:58:14 -0400

    added test case for read countries

--- src/test/java/com/w10/risk_game/utils/MapReaderTest.java
+++ src/test/java/com/w10/risk_game/utils/MapReaderTest.java
@@ -1,7 +1,16 @@
 package com.w10.risk_game.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
+
+import com.w10.risk_game.models.Continent;
+import com.w10.risk_game.models.Country;
 import com.w10.risk_game.models.GameMap;
 
 public class MapReaderTest {
@@ -13,6 +22,23 @@ public class MapReaderTest {
 		String l_mapFilename = "europe.map";
 		GameMap d_gameMap = d_mapReader.loadMapFile(l_mapFilename);
 		assertNotNull(d_gameMap);
+	}
+
+	@Test
+	public void testReadCountries() {
+		// Prepare a sample input stream with country data
+		String l_dummy_input = "1 Country1 1 1\n2 Country2 1 2\n3 Country3 2 3";
+		InputStream l_input_stream = new ByteArrayInputStream(l_dummy_input.getBytes());
+		Scanner l_input = new Scanner(l_input_stream);
+
+		// Create a map of continents for testing
+		Map<Integer, Continent> continents = Map.of(
+				1, new Continent(1, "Continent1", 5),
+				2, new Continent(2, "Continent2", 3));
+
+		Map<Integer, Country> countries = d_mapReader.readCountries(l_input, continents);
+		assertNotNull(countries);
+		assertEquals(3, countries.size());
 	}
 
 }

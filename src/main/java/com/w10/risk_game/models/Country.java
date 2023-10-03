dcae6e2dc093596c84e5357c6307b8be09a8a28c
Omnia <omnia.alam@gmail.com>
2023-10-03 16:14:00 -0400

    Update Country.java

--- src/main/java/com/w10/risk_game/models/Country.java
+++ src/main/java/com/w10/risk_game/models/Country.java
@@ -20,7 +20,7 @@ public class Country implements Serializable {
 
 	/**
 	 * This is a constructor of the country class
-	 * 
+	 *
 	 * @param p_countryId
 	 * @param p_countryName
 	 * @param p_continentId

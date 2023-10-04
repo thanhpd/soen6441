0c30e7836913f533d832ae8eb968414715906039
Darlene-Naz <46684660+Darlene-Naz@users.noreply.github.com>
2023-10-04 13:34:22 -0400

    test cleanup

--- src/test/java/com/w10/risk_game/suites/RiskGameTestSuite.java
+++ src/test/java/com/w10/risk_game/suites/RiskGameTestSuite.java
@@ -1,6 +1,5 @@
 package com.w10.risk_game.suites;
 
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -8,6 +7,5 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SuiteDisplayName("Risk Game Test Suite")
 @SelectPackages("com.w10.risk_game")
-@IncludeClassNamePatterns(".*Test")
 public class RiskGameTestSuite {
 }

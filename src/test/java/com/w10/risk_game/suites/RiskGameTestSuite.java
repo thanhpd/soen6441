d6dd29da56a0221735da5d7087f85e7ff6aa4165
Darlene Nazareth <46684660+Darlene-Naz@users.noreply.github.com>
2023-10-05 02:26:55 +0530

    added comments, javadoc for savemap ad test suite (#141)
    * completed gamemap javadoc
    
    * completed javadocs for constants
    
    * test cleanup
    
    * Country, Continent javadoc updates
    
    * added more comments for savemap and test suite

--- src/test/java/com/w10/risk_game/suites/RiskGameTestSuite.java
+++ src/test/java/com/w10/risk_game/suites/RiskGameTestSuite.java
@@ -4,6 +4,12 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
+/**
+ * The RiskGameTestSuite class is a JUnit test suite for testing the Risk Game
+ * application.
+ *
+ * @author Darlene-Naz
+ */
 @Suite
 @SuiteDisplayName("Risk Game Test Suite")
 @SelectPackages("com.w10.risk_game")

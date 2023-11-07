a12dd8ab74c1a85291129cbc7e4b72741e56fa6e
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-11-06 20:09:47 -0500

    fixed constants

--- src/main/java/com/w10/risk_game/utils/Constants.java
+++ src/main/java/com/w10/risk_game/utils/Constants.java
@@ -99,8 +99,6 @@ public final class Constants {
 	public static final String PLAYER_ISSUE_ORDER_DEPLOY_INVALID_COUNTRY = "Invalid input! The country id should be one of the countries owned by the player. Please try again.";
 	public static final String PLAYER_ISSUE_ORDER_NO_CARD = "The player does not have that card. It will not be added to the list of orders.";
 	public static final String BLOCKADE_CARD_NO_VALID_COUNTRY = "No valid country to block with ID = %s.";
-	public static final String AIRLIFT_CARD_NOT_ENOUGH_ARMIES = "Not enough armies to airlift!";
-	public static final String AIRLIFT_CARD_NO_VALID_COUNTRY = "No valid country to airlift with ID = %s.";
 	public static final String PLAYER_ISSUE_ORDER_NOT_CONTAIN_ALL_NECESSARY_PARTS = "Invalid input! The %s command should contain %s parts. Please try again.";
 	public static final String PLAYER_ISSUE_ORDER_INCORRECT = "This %s order is invalid. It will not be added to the list of orders.";
 	public static final String PLAYER_ISSUE_ORDER_SUCCEED = "This order is added to the list of orders successfully.";
@@ -185,9 +183,14 @@ public final class Constants {
 	public static final String ADVANCE_INVALID_COUNTRY_NAME = "Invalid input! Country name {0} does not exist.";
 	public static final String ADVANCE_INVALID_ARMY_LESS = "Invalid input! Number of armies must be greater than 0.";
 	public static final String ADVANCE_INVALID_ARMY_MORE = "Invalid input! Number of armies to advance cannot be greater than those deployed on the country.";
-	public static final String AIRLIFT_SUCCEED = "%s armies have been airlifed from %s to $s";
 
 	// Bomb constants
 	public static final String BOMB_SUCCEED = "Player %s bombed country %s. %s lost half of their armies.";
 	public static final String BOMB_CARD_NO_VALID_COUNTRY = "No valid country to bomb with ID = %s.";
+
+	// Airlift constants
+	public static final String AIRLIFT_SUCCEED = "%s armies have been airlifed from %s to $s";
+	public static final String AIRLIFT_CARD_NOT_ENOUGH_ARMIES = "Not enough armies to airlift!";
+	public static final String AIRLIFT_CARD_NO_VALID_COUNTRY = "No valid country to airlift with ID = %s.";
+
 }

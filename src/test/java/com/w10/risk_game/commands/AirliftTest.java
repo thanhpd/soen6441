8d495565884c9ae29421ec01088fa902b30ff7eb
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-11-07 13:11:55 -0500

    added airlift test

--- src/test/java/com/w10/risk_game/commands/AirliftTest.java
+++ src/test/java/com/w10/risk_game/commands/AirliftTest.java
@@ -1,17 +1,17 @@
-
 package com.w10.risk_game.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
-import com.w10.risk_game.commands.Airlift;
 import com.w10.risk_game.models.Country;
 import com.w10.risk_game.models.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test class for the Airlift command.
+ */
 public class AirliftTest {
 	private Order d_order1;
 	private Country country1;
@@ -21,39 +21,44 @@ public class AirliftTest {
 	private int country1InitialArmyCount = 20;
 	private int country2InitialArmyCount = 12;
 
+	/**
+	 * Set up initial test conditions.
+	 */
 	@BeforeEach
 	public void setUp() {
+		// Create two countries
 		country1 = new Country(1, "England", 1, country1InitialArmyCount);
 		country2 = new Country(2, "France", 1, country2InitialArmyCount);
-		country1.addNeighbor(country2);
-		country2.addNeighbor(country1);
 
+		// Create a player
 		player1 = new Player("Player1", new ArrayList<>(), new ArrayList<>(), 10);
 
+		// Create a list of countries and assign them to the player
 		ArrayList<Country> countries1 = new ArrayList<>() {
 			{
 				add(country1);
-			}
-		};
-		ArrayList<Country> countries2 = new ArrayList<>() {
-			{
 				add(country2);
 			}
 		};
-		// set country-1 and country-2 owner as player 1
+
+		// Set country-1 and country-2 owner as player 1
 		country1.setOwner(player1);
 		player1.setCountriesOwned(countries1);
-
-		country2.setOwner(player1);
-		player1.setCountriesOwned(countries2);
 	}
 
+	/**
+	 * The testAirliftExecution function creates an Airlift order, executes it, and
+	 * checks if the
+	 * number of armies in country1 has been updated as expected.
+	 */
 	@Test
 	public void testAirliftExecution() {
+		// Create an Airlift order
 		d_order1 = new Airlift(player1, "1", "2", "5");
+		// Execute the Airlift order
 		d_order1.execute();
 
+		// Check if the number of armies in country1 has been updated as expected
 		assertEquals(15, country1.getArmyCount());
-
 	}
 }

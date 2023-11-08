0e8fdeb7e44a840cbd246d85b9aef96088f38001
Thanh Phan <ph_thanh@live.concordia.ca>
2023-11-08 16:50:44 -0500

    doc: update javadoc for build 2

--- docs/apidocs/testapidocs/package-search-index.js
+++ docs/apidocs/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.w10.risk_game.models"},{"l":"com.w10.risk_game.suites"},{"l":"com.w10.risk_game.utils"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.w10.risk_game.commands"},{"l":"com.w10.risk_game.controllers"},{"l":"com.w10.risk_game.models"},{"l":"com.w10.risk_game.models.phases"},{"l":"com.w10.risk_game.suites"},{"l":"com.w10.risk_game.utils"},{"l":"com.w10.risk_game.utils.loggers"}]

0d4b6ef19c6d9dc8533cc339a99d7edb0cc7fe0c
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-11-07 19:39:02 -0500

    fixed formatting

--- src/main/java/com/w10/risk_game/commands/Bomb.java
+++ src/main/java/com/w10/risk_game/commands/Bomb.java
@@ -23,9 +23,9 @@ public class Bomb extends Order {
 	 * Constructor for Bomb class.
 	 *
 	 * @param p_player
-	 *                    The player who is issuing the order.
+	 *            The player who is issuing the order.
 	 * @param p_countryId
-	 *                    The country id of the country to bomb.
+	 *            The country id of the country to bomb.
 	 */
 	public Bomb(Player p_player, String p_countryId) {
 		this.d_player = p_player;
@@ -54,13 +54,11 @@ public class Bomb extends Order {
 	 * Checks if a player can bomb a specific country.
 	 *
 	 * @param p_player
-	 *                    The player object that represents the player who is trying
-	 *                    to
-	 *                    validate the order.
+	 *            The player object that represents the player who is trying to
+	 *            validate the order.
 	 * @param p_countryId
-	 *                    The country ID identifying the country that the player
-	 *                    wants to
-	 *                    bomb.
+	 *            The country ID identifying the country that the player wants to
+	 *            bomb.
 	 * @return The method is returning a boolean value.
 	 */
 	public static boolean ValidateOrder(Player p_player, String p_countryId) {
@@ -74,12 +72,10 @@ public class Bomb extends Order {
 	 * object that the player can bomb, based on the given country ID.
 	 *
 	 * @param p_player
-	 *                    The player object for which we want to find a country to
-	 *                    bomb.
+	 *            The player object for which we want to find a country to bomb.
 	 * @param p_countryId
-	 *                    The parameter "p_countryId" is a String representing the
-	 *                    ID of the
-	 *                    country to be bombed.
+	 *            The parameter "p_countryId" is a String representing the ID of the
+	 *            country to be bombed.
 	 * @return The method is returning a Country object.
 	 */
 	public static Country GetCountryToBomb(Player p_player, String p_countryId) {
@@ -119,8 +115,8 @@ public class Bomb extends Order {
 	 * are neighbors of the countries owned by the given player.
 	 *
 	 * @param p_player
-	 *                 The parameter "p_player" is of type Player and represents the
-	 *                 player for whom we want to find the foreign neighbors.
+	 *            The parameter "p_player" is of type Player and represents the
+	 *            player for whom we want to find the foreign neighbors.
 	 * @return The method is returning a list of countries that are owned by other
 	 *         players and are neighbors of the countries owned by the given player.
 	 */
@@ -150,7 +146,7 @@ public class Bomb extends Order {
 	 * This function is used to check the input format for airlift command.
 	 *
 	 * @param p_inputArray
-	 *                     the input string split by space
+	 *            the input string split by space
 	 * @return boolean value to show whether the input format is valid
 	 */
 	public static boolean CheckValidBombInput(String[] p_inputArray) {

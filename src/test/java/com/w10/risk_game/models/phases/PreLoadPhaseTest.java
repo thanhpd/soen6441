0054bfc6a0cccbba49247dd7cc9f383cfea550ba
Omnia Alam <31718191+AlamOmnia@users.noreply.github.com>
2023-11-08 15:48:57 -0500

    Dev test case phase (#273)
    * Pre-Load Phase testcase
    
    * Update PreLoadPhaseTest.java
    
    * Postload testCases
    
    * PlaySetup TestCase
    
    * apply spotless
    
    * Update PlaySetupPhaseTest.java

---
+++ src/test/java/com/w10/risk_game/models/phases/PreLoadPhaseTest.java
@@ -0,0 +1,72 @@
+package com.w10.risk_game.models.phases;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.w10.risk_game.GameEngine;
+import com.w10.risk_game.controllers.MapEditorController;
+import com.w10.risk_game.models.Phase;
+import com.w10.risk_game.utils.Constants;
+
+/**
+ * The PreLoadPhaseTest class tests the functionality of the PreLoadPhase class
+ * in a game engine.
+ */
+public class PreLoadPhaseTest {
+
+	Phase phase;
+
+	/**
+	 * The setup function initializes a GameEngine object and sets its phase to a
+	 * PreLoadPhase.
+	 */
+	@BeforeEach
+	public void setup() {
+		GameEngine l_GameEngine = new GameEngine();
+		GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
+		phase = GameEngine.Phase;
+	}
+
+	/**
+	 * The testPhaseName function tests that the phase name is "PreLoad PHASE".
+	 */
+	@Test
+	void testPhaseName() {
+		assertEquals("PreLoad PHASE", phase.getPhaseName());
+
+	}
+
+	/**
+	 * The testAvailableCommands function tests that the available commands in the
+	 * phase object are correctly returned as a string.
+	 */
+	@Test
+	void testAvailableCommands() {
+		assertEquals("[loadmap]", phase.getAvailableCommands().toString());
+
+	}
+
+	/**
+	 * The testNextIfMapValid function tests if the phase loads a valid map
+	 * correctly.
+	 */
+	@Test
+	void testNextIfMapValid() {
+		phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "europe.map");
+		phase = GameEngine.Phase;
+		assertEquals("PostLoad PHASE", GameEngine.Phase.getPhaseName());
+	}
+
+	/**
+	 * The "end" function sets the "phase" variable to null.
+	 */
+	@AfterEach
+	public void end() {
+		phase = null;
+	}
+
+}

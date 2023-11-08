1122eb89157870be98edc24cf72eb71133fde9e1
Omnia <omnia.alam@gmail.com>
2023-11-08 15:32:47 -0500

    apply spotless

--- src/test/java/com/w10/risk_game/models/phases/PlaySetupPhaseTest.java
+++ src/test/java/com/w10/risk_game/models/phases/PlaySetupPhaseTest.java
@@ -3,6 +3,7 @@ package com.w10.risk_game.models.phases;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,57 +12,64 @@ import com.w10.risk_game.models.Phase;
 import com.w10.risk_game.utils.Constants;
 
 /**
- * The PlaySetupPhaseTest class is a JUnit test class that tests the functionality of the
- * PlaySetupPhase class in a game engine.
+ * The PlaySetupPhaseTest class is a JUnit test class that tests the
+ * functionality of the PlaySetupPhase class in a game engine.
  */
 public class PlaySetupPhaseTest {
 
-      Phase phase;
-  GameEngine l_GameEngine;
+	Phase phase;
+	GameEngine l_GameEngine;
 
+	/**
+	 * The setup function initializes a GameEngine object, sets the phase to
+	 * PreLoadPhase, loads a map, and advances to the next phase.
+	 */
+	@BeforeEach
+	public void setup() {
+		l_GameEngine = new GameEngine();
+		GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
+		phase = GameEngine.Phase;
+		phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "europe.map");
+		phase = GameEngine.Phase;
+		phase.nextPhase();;
+		phase = GameEngine.Phase;
+	}
 
-   /**
-    * The setup function initializes a GameEngine object, sets the phase to PreLoadPhase, loads a map,
-    * and advances to the next phase.
-    */
-    @BeforeEach
-    public void setup(){
-    l_GameEngine= new GameEngine();
-    GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
-    phase=GameEngine.Phase;
-    phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "europe.map");
-    phase=GameEngine.Phase;
-    phase.nextPhase();;
-    phase= GameEngine.Phase;
-    }
+	/**
+	 * The testGetAvailableCommands function checks if the available commands list
+	 * contains the "assigncountries" command.
+	 */
+	@Test
+	void testGetAvailableCommands() {
+		assertTrue(phase.getAvailableCommands().toString().contains("assigncountries"));
+	}
+	/**
+	 * The testPhaseName function tests that the phase name is correctly returned as
+	 * "PlaySetup PHASE".
+	 */
+	@Test
+	void testPhaseName() {
+		assertEquals("PlaySetup PHASE", phase.getPhaseName());
 
-   /**
-    * The testGetAvailableCommands function checks if the available commands list contains the
-    * "assigncountries" command.
-    */
-    @Test
-    void testGetAvailableCommands() {
- assertTrue(phase.getAvailableCommands().toString().contains("assigncountries") );
-    }
-/**
- * The testPhaseName function tests that the phase name is correctly returned as "PlaySetup PHASE".
- */
- @Test
-    void testPhaseName() {
-        assertEquals("PlaySetup PHASE", phase.getPhaseName());
+	}
 
-    }
+	/**
+	 * The testNext function tests the next phase of the game engine.
+	 */
+	@Test
+	void testNext() {
+		phase.createPlayer("player1");
+		phase.createPlayer("player2");
+		phase.assignCountries();
+		phase = GameEngine.Phase;
+		assertEquals("Reinforcement PHASE", phase.getPhaseName());
 
-/**
- * The testNext function tests the next phase of the game engine.
- */
-    @Test
-    void testNext() {
-phase.createPlayer("player1");
-phase.createPlayer("player2");
-phase.assignCountries();
-phase= GameEngine.Phase;
-assertEquals("Reinforcement PHASE", phase.getPhaseName());
-
-    }
+	}
+	/**
+	 * The "end" function sets the "phase" variable to null.
+	 */
+	@AfterEach
+	public void end() {
+		phase = null;
+	}
 }
--- src/test/java/com/w10/risk_game/models/phases/PostLoadPhaseTest.java
+++ src/test/java/com/w10/risk_game/models/phases/PostLoadPhaseTest.java
@@ -3,6 +3,7 @@ package com.w10.risk_game.models.phases;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,65 +12,73 @@ import com.w10.risk_game.models.Phase;
 import com.w10.risk_game.utils.Constants;
 
 public class PostLoadPhaseTest {
-  Phase phase;
-  GameEngine l_GameEngine;
+	Phase phase;
+	GameEngine l_GameEngine;
 
-    /**
-     * The setup function initializes a GameEngine object and sets its phase to a PostLoadPhase.
-     */
-    @BeforeEach
-    public void setup(){
+	/**
+	 * The setup function initializes a GameEngine object and sets its phase to a
+	 * PostLoadPhase.
+	 */
+	@BeforeEach
+	public void setup() {
 
-            l_GameEngine= new GameEngine();
-            GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
-            phase=GameEngine.Phase;
-            phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "europe.map");
-            phase=GameEngine.Phase;
-            
-        }
+		l_GameEngine = new GameEngine();
+		GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
+		phase = GameEngine.Phase;
+		phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "europe.map");
+		phase = GameEngine.Phase;
 
+	}
 
-       /**
-     * The testPhaseName function tests that the phase name is "PostLoad PHASE".
-     */
-    @Test
-    void testPhaseName() {
-        assertEquals("PostLoad PHASE", phase.getPhaseName());
+	/**
+	 * The testPhaseName function tests that the phase name is "PostLoad PHASE".
+	 */
+	@Test
+	void testPhaseName() {
+		assertEquals("PostLoad PHASE", phase.getPhaseName());
 
-    }
-/**
- * The testGetAvailableCommands function checks if the available commands list contains the
- * "editcountry" command.
- */
+	}
+	/**
+	 * The testGetAvailableCommands function checks if the available commands list
+	 * contains the "editcountry" command.
+	 */
 
-    @Test
-    void testGetAvailableCommands() {
-        assertTrue(phase.getAvailableCommands().toString().contains("editcountry") );
-    }
+	@Test
+	void testGetAvailableCommands() {
+		assertTrue(phase.getAvailableCommands().toString().contains("editcountry"));
+	}
 
+	/**
+	 * The testNextPhaseIfMapValid function tests if the nextPhase method correctly
+	 * updates the phase to "PlaySetup PHASE".
+	 */
+	@Test
+	void testNextPhaseIfMapValid() {
+		phase.nextPhase();
+		phase = GameEngine.Phase;
+		assertEquals("PlaySetup PHASE", phase.getPhaseName());
+	}
+	/**
+	 * The testNextIfMapInvalid function tests if the nextPhase method in the
+	 * PostLoadPhase class correctly sets the phase to PostLoadPhase when an invalid
+	 * map is loaded.
+	 */
+	@Test
+	void testNextIfMapInvalid() {
+		GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
+		phase = GameEngine.Phase;
+		phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "test24.map");
+		phase = GameEngine.Phase;
+		phase.nextPhase();
+		phase = GameEngine.Phase;
+		assertEquals("PreLoad PHASE", phase.getPhaseName());
+	}
 
-   /**
-    * The testNextPhaseIfMapValid function tests if the nextPhase method correctly updates the phase to
-    * "PlaySetup PHASE".
-    */
-    @Test
-    void testNextPhaseIfMapValid() {
-        phase.nextPhase();
-        phase=GameEngine.Phase;
-        assertEquals("PlaySetup PHASE", phase.getPhaseName());
-    }
-/**
- * The testNextIfMapInvalid function tests if the nextPhase method in the PostLoadPhase class correctly
- * sets the phase to PostLoadPhase when an invalid map is loaded.
- */
-@Test
-void testNextIfMapInvalid(){
-    GameEngine.SetPhase(new PreLoadPhase(l_GameEngine));
-    phase=GameEngine.Phase;
-    phase.loadMap(Constants.DEFAULT_GAME_MAP_TEST_FOLDER_PATH + "test24.map");
-    phase=GameEngine.Phase;
-    phase.nextPhase();
-    phase=GameEngine.Phase;
-    assertEquals("PreLoad PHASE", phase.getPhaseName());
-}
+	/**
+	 * The "end" function sets the "phase" variable to null.
+	 */
+	@AfterEach
+	public void end() {
+		phase = null;
+	}
 }

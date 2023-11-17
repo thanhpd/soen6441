0ac341b0fa63f5e1550dcad5b3dc6ea3fed78526
YajingLiu2357 <79319316+YajingLiu2357@users.noreply.github.com>
2023-11-17 14:43:07 -0500

    save and load map #281

--- src/main/java/com/w10/risk_game/models/DataStorage.java
+++ src/main/java/com/w10/risk_game/models/DataStorage.java
@@ -12,10 +12,22 @@ public class DataStorage implements Serializable {
 	// Phase
 	// TODO: Phase Data;
 	// Map
-	// TODO: Map Data;
+	// Country
+	public ArrayList<Integer> d_countryIds = new ArrayList<Integer>();
+	public ArrayList<String> d_countryNames = new ArrayList<String>();
+	public ArrayList<Integer> d_belongContinentIds = new ArrayList<Integer>();
+	public ArrayList<Integer> d_armyCounts = new ArrayList<Integer>();
+	public ArrayList<String> d_ownerNames = new ArrayList<String>();
+	public ArrayList<Integer> d_numberOfNeighbors = new ArrayList<Integer>();
+	public ArrayList<Integer> d_neighborIds = new ArrayList<Integer>();
+	// Continent
+	public ArrayList<Integer> d_continentIds = new ArrayList<Integer>();
+	public ArrayList<String> d_continentNames = new ArrayList<String>();
+	public ArrayList<Integer> d_continentBonus = new ArrayList<Integer>();
 	// Player
 	public ArrayList<String> d_playerNames = new ArrayList<String>();
 	public ArrayList<Integer> d_numberOfCountries = new ArrayList<Integer>();
+	public ArrayList<Integer> d_ownedCountryIds = new ArrayList<Integer>();
 	public ArrayList<Integer> d_numberOfOrders = new ArrayList<Integer>();
 	public ArrayList<Integer> d_playerLeftoverArmies = new ArrayList<Integer>();
 	public ArrayList<Integer> d_numberOfCards = new ArrayList<Integer>();
--- src/main/java/com/w10/risk_game/utils/SaveLoad.java
+++ src/main/java/com/w10/risk_game/utils/SaveLoad.java
@@ -10,7 +10,9 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * The SaveLoad class is responsible for saving and loading the game.
@@ -22,6 +24,8 @@ public class SaveLoad {
 	GamePlayController d_gamePlayController;
 	DataStorage d_data;
 	List<Player> d_playerList;
+	Map<Integer, Country> d_countries;
+	GameMap d_gameMap;
 	/**
 	 * Constructor for SaveLoad class.
 	 * @param p_gameEngine The parameter `p_gameEngine` is a GameEngine object.
@@ -33,6 +37,7 @@ public class SaveLoad {
 		// TODO: Get Global PlayerList
 		this.d_playerList = p_gameEngine.getGame().GetPlayerListForDiplomacy();
 		// TODO: Get Global GameMap
+		this.d_gameMap = p_gameEngine.getMapEditorController().getGameMap();
 	}
 
 	/**
@@ -43,8 +48,24 @@ public class SaveLoad {
 		try{
 			ObjectOutputStream l_out = new ObjectOutputStream(new FileOutputStream("save.dat"));
 			d_data = new DataStorage();
-			// TODO: Get GameMap Data
-
+			// Save Country Data
+			for (Map.Entry<Integer,Country> entry : d_gameMap.getCountries().entrySet()) {
+				d_data.d_countryIds.add(entry.getKey());
+				d_data.d_countryNames.add(entry.getValue().getCountryName());
+				d_data.d_belongContinentIds.add(entry.getValue().getContinentId());
+				d_data.d_armyCounts.add(entry.getValue().getArmyCount());
+				d_data.d_ownerNames.add(entry.getValue().getOwner().getName());
+				d_data.d_numberOfNeighbors.add(entry.getValue().getNeighbors().size());
+				for (Map.Entry<Integer, Country> neighbor: entry.getValue().getNeighbors().entrySet()){
+					d_data.d_neighborIds.add(neighbor.getValue().getCountryId());
+				}
+			}
+			// Save Continent Data
+			for (Map.Entry<Integer,Continent> entry : d_gameMap.getContinents().entrySet()) {
+				d_data.d_continentIds.add(entry.getKey());
+				d_data.d_continentNames.add(entry.getValue().getContinentName());
+				d_data.d_continentBonus.add(entry.getValue().getBonus());
+			}
 			// Save Player Data
 			for (Player player : d_playerList) {
 				d_data.d_playerNames.add(player.getName());
@@ -58,7 +79,9 @@ public class SaveLoad {
 				}
 				// Save Country Data
 				d_data.d_numberOfCountries.add(player.getCountriesOwned().size());
-				// TODO: Get Player CountryList
+				for (Country country : player.getCountriesOwned()) {
+					d_data.d_ownedCountryIds.add(country.getCountryId());
+				}
 				// Save Card Data
 				d_data.d_numberOfCards.add(player.getPlayerCards().size());
 				for (CardType card : player.getPlayerCards()) {
@@ -79,15 +102,53 @@ public class SaveLoad {
 		try {
 			ObjectInputStream l_in = new ObjectInputStream(getClass().getResourceAsStream("save.dat"));
 			d_data = (DataStorage) l_in.readObject();
-			// TODO: Get GameMap Data
+			// Load Country Data
+			ArrayList <Integer> l_countryIds = d_data.d_countryIds;
+			ArrayList <String> l_countryNames = d_data.d_countryNames;
+			ArrayList <Integer> l_belongContinentIds = d_data.d_belongContinentIds;
+			ArrayList <Integer> l_armyCounts = d_data.d_armyCounts;
+			ArrayList <String> l_ownerNames = d_data.d_ownerNames;
+			ArrayList <Integer> l_numberOfNeighbors = d_data.d_numberOfNeighbors;
+			ArrayList <Integer> l_neighborIds = d_data.d_neighborIds;
+			int l_countryNumber = l_countryIds.size();
+			d_countries = new HashMap<>();
+			for (int i = 0; i < l_countryNumber; i++){
+				Country l_country = new Country(l_countryIds.get(i), l_countryNames.get(i), l_belongContinentIds.get(i), l_armyCounts.get(i));
+				d_countries.put(l_countryIds.get(i), l_country);
+			}
+			for (int i = 0; i < l_countryNumber; i++){
+				Country l_country = d_countries.get(l_countryIds.get(i));
+				int l_currentCountryNumberOfNeighbors = l_numberOfNeighbors.get(i);
+				for (int j = 0; j < l_currentCountryNumberOfNeighbors; j++){
+					Country l_neighbor = d_countries.get(l_neighborIds.remove(0));
+					l_country.addNeighbor(l_neighbor);
+				}
+			}
+			// Load Continent Data
+			ArrayList <Integer> l_continentIds = d_data.d_continentIds;
+			ArrayList <String> l_continentNames = d_data.d_continentNames;
+			ArrayList <Integer> l_continentBonus = d_data.d_continentBonus;
+			int l_continentNumber = l_continentIds.size();
+			Map<Integer, Continent> l_continents = new HashMap<>();
+			for (int i = 0; i < l_continentNumber; i++){
+				Continent l_continent = new Continent(l_continentIds.get(i), l_continentNames.get(i), l_continentBonus.get(i));
+				l_continents.put(l_continentIds.get(i), l_continent);
+			}
+			for (int i = 0; i < l_countryNumber; i++){
+				Country l_country = d_countries.get(l_countryIds.get(i));
+				Continent l_continent = l_continents.get(l_belongContinentIds.get(i));
+				l_continent.addCountry(l_country);
+			}
 			// Load Player Data
 			ArrayList <String> l_playerNames = d_data.d_playerNames;
 			ArrayList <Integer> l_playerLeftoverArmies = d_data.d_playerLeftoverArmies;
 			ArrayList <Boolean> l_playerHasCommitted = d_data.d_playerHasCommitted;
 			ArrayList <Integer> l_numberOfOrders = d_data.d_numberOfOrders;
 			ArrayList <Integer> l_numberOfCountries = d_data.d_numberOfCountries;
+			ArrayList <Integer> l_ownedCountryIds = d_data.d_ownedCountryIds;
 			ArrayList <Integer> l_numberOfCards = d_data.d_numberOfCards;
 			int l_playerNumber = l_playerNames.size();
+			List<Player> l_playerList = new ArrayList<>();
 			for (int i = 0; i < l_playerNumber; i++){
 				Player l_player = new Player(l_playerNames.get(i), new ArrayList<>(), new ArrayList<>(), l_playerLeftoverArmies.get(i), new ArrayList<>(), l_playerHasCommitted.get(i));
 				// Load Order Data
@@ -95,13 +156,20 @@ public class SaveLoad {
 				List <Order> l_orderList = loadOrder(l_player, l_currentPlayerNumberOfOrders);
 				l_player.setOrders(l_orderList);
 				// Load Country Data
-				// TODO: Get Player CountryList
+				int l_currentPlayerNumberOfCountries = l_numberOfCountries.get(i);
+				for (int j = 0; j < l_currentPlayerNumberOfCountries; j++){
+					Country l_country = d_countries.get(l_ownedCountryIds.remove(0));
+					l_country.setOwner(l_player);
+					l_player.addCountry(l_country);
+				}
 				// Load Card Data
 				int l_currentPlayerNumberOfCards = l_numberOfCards.get(i);
 				List <CardType> l_cardList = loadCard(l_currentPlayerNumberOfCards);
 				l_player.setPlayerCards(l_cardList);
+				l_playerList.add(l_player);
 			}
 			// TODO: Set Global PlayerList
+			// TODO: Set Global GameMap
 
 		}catch (Exception e){
 			System.out.println("Cannot load the game");
@@ -168,8 +236,10 @@ public class SaveLoad {
 					int l_countryFromId = Integer.parseInt(d_data.d_advanceCountryFromIds.remove(0));
 					int l_countryToId = Integer.parseInt(d_data.d_advanceCountryToIds.remove(0));
 					int l_numOfArmies = d_data.d_advanceNums.remove(0);
-					// TODO: Get Country Using CountryId
-					// TODO: Create Advance Order
+					Country l_countryFrom = d_countries.get(l_countryFromId);
+					Country l_countryTo = d_countries.get(l_countryToId);
+					Order l_advance = new Advance(l_countryFrom, l_countryTo, l_numOfArmies);
+					l_orders.add(l_advance);
 					break;
 				}
 				case "Airlift": {

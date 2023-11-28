18d652c05ff95e2b7aa3137b5e37c65c775953c3
Omnia <omnia.alam@gmail.com>
2023-11-28 16:22:10 -0500

    Revert "merge"
    This reverts commit 7860a69ba32c697d49a710569d312ffdc018b7f1.

---
+++ src/main/java/com/w10/risk_game/models/tournament/TournamentOptions.java
@@ -0,0 +1,78 @@
+package com.w10.risk_game.models.tournament;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TournamentOptions {
+
+	/**
+	 * list for Map
+	 */
+	private List<String> d_Map = new ArrayList<>();
+	/**
+	 * List to hold player strategy
+	 */
+	private List<String> d_PlayerStrategies = new ArrayList<>();
+	/**
+	 * number of games
+	 */
+	private int d_Games;
+	/**
+	 * maximum number of tries
+	 */
+	private int d_MaxTries;
+
+	/**
+	 * method to get map
+	 * 
+	 * @return the map
+	 */
+	public List<String> getMap() {
+		return d_Map;
+	}
+
+	/**
+	 * method to get player strategies
+	 *
+	 * @return player strategies
+	 */
+	public List<String> getPlayerStrategies() {
+		return d_PlayerStrategies;
+	}
+
+	/**
+	 * method to get games
+	 * 
+	 * @return number of games
+	 */
+	public int getGames() {
+		return d_Games;
+	}
+
+	/**
+	 * method to set game
+	 * 
+	 * @param p_Games number of games
+	 */
+	public void setGames(int p_Games) {
+		d_Games = p_Games;
+	}
+
+	/**
+	 * method to get maximum tried per game
+	 *
+	 * @return the maximum tries
+	 */
+	public int getMaxTries() {
+		return d_MaxTries;
+	}
+
+	/**
+	 * method to set maximum tries
+	 * 
+	 * @param p_MaxTries maximum tries
+	 */
+	public void setMaxTries(int p_MaxTries) {
+		d_MaxTries = p_MaxTries;
+	}
+}
